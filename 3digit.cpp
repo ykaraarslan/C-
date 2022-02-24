f1e7a718e16d550507fa7017afeffc0a27f2851c
Yiğitcan Karaarslan <93783187+ykaraarslan@users.noreply.github.com>
2022-02-24 15:35:17 +0300

    Add files via upload

---
+++ 3digit.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	int digit1, digit10, digit100, total, number;
+	total = 0;
+	printf("Enter 3 digit number:");
+	scanf_s("%d", &number);
+
+	digit1 = number % 10;
+	digit10 = (number / 10) % 10;
+	digit100 = number / 100;
+	total = digit1 + digit10 + digit100;
+	printf("%d", total);
+	return 0;
+}
